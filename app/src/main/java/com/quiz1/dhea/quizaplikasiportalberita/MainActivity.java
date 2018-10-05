59a84caaf27a386e423b5681eefaa85ad3570e81
dhearizky12 <dherizdhea@gmail.com>
2018-10-06 00:59:12 +0700

    first commit

---
+++ app/src/main/java/com/quiz1/dhea/quizaplikasiportalberita/MainActivity.java
@@ -0,0 +1,29 @@
+package com.quiz1.dhea.quizaplikasiportalberita;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void readmore1 (View view) {
+        Intent intent = new Intent( this, berita1.class);
+        startActivity(intent);
+
+    }
+    public void readmore2 (View view) {
+        Intent intent = new Intent(this, berita2.class);
+        startActivity(intent);
+    }
+    public void readmore3 (View view) {
+        Intent intent = new Intent( this, berita3.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/quiz1/dhea/quizaplikasiportalberita/berita1.java
@@ -0,0 +1,15 @@
+package com.quiz1.dhea.quizaplikasiportalberita;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class berita1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_berita1);
+    }
+}
